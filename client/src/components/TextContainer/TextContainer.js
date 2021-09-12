bbdfd86866025b570a247cdea63cd797a7f2cb47
M-AyanShaikh <89039969+M-AyanShaikh@users.noreply.github.com>
2021-09-12 16:26:26 -0400

    Add files via upload

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    <div>
+      <h1>ChitChat.io <span role="img" aria-label="emoji">🗣️</span></h1>
+      <h2>Front-End created using React</h2>
+      <h2>Back-End created using Express + Socket.IO</h2>
+    </div>
+    {
+      users
+        ? (
+          <div>
+            <h1>People currently chatting:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    {name}
+                    <img alt="Online Icon" src={onlineIcon}/>
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
